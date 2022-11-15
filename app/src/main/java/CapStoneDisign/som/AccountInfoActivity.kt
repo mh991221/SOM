33eb7f806a9b7466fa0ddc8db06a83bd7721a365
mh991221 <mh991221@naver.com>
2022-11-16 02:25:38 +0900

    upload image uri

--- app/src/main/java/CapStoneDisign/som/AccountInfoActivity.kt
+++ app/src/main/java/CapStoneDisign/som/AccountInfoActivity.kt
@@ -5,24 +5,31 @@ import CapStoneDisign.som.DBKey.Companion.DB_USERS
 import CapStoneDisign.som.Model.GroupModel
 import CapStoneDisign.som.Model.UserModel
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.DatePickerDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
-import com.google.firebase.database.ktx.snapshots
 import com.google.firebase.ktx.Firebase
-import org.w3c.dom.Text
-import java.text.SimpleDateFormat
-import java.util.Calendar
+import com.google.firebase.storage.FirebaseStorage
+import java.util.*
+
 
 class AccountInfoActivity : AppCompatActivity() {
 
@@ -42,9 +49,6 @@ class AccountInfoActivity : AppCompatActivity() {
         findViewById(R.id.editPhotoButton)
     }
 
-    private val editPartnerPhotoButton: Button by lazy{
-        findViewById(R.id.editPartnerPhotoButton)
-    }
 
     private val auth: FirebaseAuth by lazy {
         Firebase.auth
@@ -60,9 +64,19 @@ class AccountInfoActivity : AppCompatActivity() {
         Firebase.database.reference.child(DB_GROUPS)
     }
 
+    private val accountInfoImageView :ImageView by lazy{
+        findViewById(R.id.accountInfoImageView)
+    }
+    private val accountInfoImageViewPartner :ImageView by lazy{
+        findViewById(R.id.accountInfoImageViewPartner)
+    }
+
     var userModel: UserModel? = null
+    var userModel2: UserModel?=null
     var groupModel: GroupModel? = null
+
     var count: Int = 0
+    lateinit var storage: FirebaseStorage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -97,13 +111,145 @@ class AccountInfoActivity : AppCompatActivity() {
         userDB.child(user).addValueEventListener(postListener)*/
 
 
-        initPhoto()
+
         initGroup()
         initDate()
+        initPhoto()
     }
 
     private fun initPhoto(){
 
+        editPhotoButton.setOnClickListener {
+            when{
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED ->{
+                    navigatePhotos()
+                }
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) ->{
+                    showPermissionContextPopup()
+                }
+                else ->{
+                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+                }
+            }
+        }
+
+
+        var partnerId: String?=null
+        val curruser = userDB.child(user)
+        curruser.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                userModel = snapshot.getValue<UserModel>()
+
+                val currentGroup = groupDB.child(userModel?.groupID!!) // groupId에 접근
+                currentGroup.addValueEventListener(object: ValueEventListener{
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        groupModel = snapshot.getValue<GroupModel>()
+
+                        Log.d("checkiing partner","${groupModel?.secondUserID}")
+
+                        if(user.compareTo(groupModel?.firstUserID!!) == 0){     // 내 id와 대조하여 상대의 id 찾아내기
+                            partnerId = groupModel?.secondUserID.toString()
+                            Log.d("checkiing partner","${partnerId}")
+
+                        }else{
+                            partnerId = groupModel?.firstUserID.toString()
+
+                        }
+
+                        Log.d("checkiing partner","${partnerId}")
+                        val storage = FirebaseStorage.getInstance()
+                        val storageRef = storage.reference
+
+                        storageRef.child("image").child(user).downloadUrl.addOnSuccessListener { uri -> //이미지 로드 성공시
+                            Glide.with(applicationContext)
+                                .load(uri)
+                                .into(accountInfoImageView)
+                        }.addOnFailureListener { //이미지 로드 실패시
+                        }
+
+                        storageRef.child("image").child(partnerId!!).downloadUrl.addOnSuccessListener { uri -> //이미지 로드 성공시
+                            Glide.with(applicationContext)
+                                .load(uri)
+                                .into(accountInfoImageViewPartner)
+                        }.addOnFailureListener { //이미지 로드 실패시
+                        }
+
+                    }
+                    override fun onCancelled(error: DatabaseError) {}
+                })
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })
+
+
+
+
+
+
+    }
+
+    private fun navigatePhotos(){
+        val intent  = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent,2000)
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        storage= FirebaseStorage.getInstance()
+        if(resultCode != Activity.RESULT_OK){
+            return
+        }
+        when(requestCode){
+            2000 ->{
+                val selectedImageUri : Uri? = data?.data
+
+                if(selectedImageUri != null){
+
+                    Log.d("checkingPhoto","${selectedImageUri}")
+                    selectedImageUri.let{
+
+                        Glide.with(this)
+                            .load(selectedImageUri)
+                            .fitCenter()
+                            .apply(RequestOptions().override(500,500))
+                            .into(accountInfoImageView)
+                    }
+
+                    var fileName = user
+                    storage.getReference().child("image").child(fileName).delete()
+                    storage.getReference().child("image").child(fileName)
+                        .putFile(selectedImageUri)
+
+
+
+                }else{
+                    Toast.makeText(this,"사진을 가져오지 못했습니다.",Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            else ->{
+                Toast.makeText(this,"사진을 가져오지 못했습니다.",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+
+    private fun showPermissionContextPopup(){
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다")
+            .setMessage("앱에서 사진을 불러오기 위해 권한이 필요합니다")
+            .setPositiveButton("동의하기") {_, _->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+            }
+            .setNegativeButton("취소하기") {_, _-> }
+            .create()
+            .show()
     }
 
     private fun initGroup(){
@@ -112,6 +258,7 @@ class AccountInfoActivity : AppCompatActivity() {
         val phoneNumberTextView = findViewById<TextView>(R.id.phoneNumberTextView)
         val nameTextViewPartner = findViewById<TextView>(R.id.nameTextViewPartner)
 
+        var partnerId: String?=null
         val curruser = userDB.child(user)
         curruser.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -125,7 +272,7 @@ class AccountInfoActivity : AppCompatActivity() {
                 currentGroup.addValueEventListener(object: ValueEventListener{
                     override fun onDataChange(snapshot: DataSnapshot) {
                         groupModel = snapshot.getValue<GroupModel>()
-                        var partnerId: String?=null
+
 
                         partnerId = if(user.compareTo(groupModel?.firstUserID!!) == 0){     // 내 id와 대조하여 상대의 id 찾아내기
                             groupModel?.secondUserID.toString()
@@ -133,12 +280,12 @@ class AccountInfoActivity : AppCompatActivity() {
                             groupModel?.firstUserID.toString()
                         }
 
-                        val partnerUser = userDB.child(partnerId)                       // 파트너의 id에 접근하여 이름 알아내기
+                        val partnerUser = userDB.child(partnerId!!)                       // 파트너의 id에 접근하여 이름 알아내기
                         partnerUser.addValueEventListener(object : ValueEventListener{
                             override fun onDataChange(snapshot: DataSnapshot) {
-                                userModel = snapshot.getValue<UserModel>()
+                                userModel2 = snapshot.getValue<UserModel>()
 
-                                nameTextViewPartner.text = userModel?.name
+                                nameTextViewPartner.text = userModel2?.name
                             }
                             override fun onCancelled(error: DatabaseError) {}
                         })
@@ -265,6 +412,7 @@ class AccountInfoActivity : AppCompatActivity() {
         }
 
     }
+
 
 
 }
