18e593e1200344b80d8b94c6bb67f867226cf5a4
mh991221 <mh991221@naver.com>
2022-11-15 22:50:39 +0900

    accontInfo updated

--- app/src/main/java/CapStoneDisign/som/AccountInfoActivity.kt
+++ app/src/main/java/CapStoneDisign/som/AccountInfoActivity.kt
@@ -1,7 +1,11 @@
 package CapStoneDisign.som
 
+import CapStoneDisign.som.DBKey.Companion.DB_GROUPS
 import CapStoneDisign.som.DBKey.Companion.DB_USERS
+import CapStoneDisign.som.Model.GroupModel
 import CapStoneDisign.som.Model.UserModel
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -16,6 +20,9 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.database.ktx.snapshots
 import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 class AccountInfoActivity : AppCompatActivity() {
 
@@ -25,6 +32,18 @@ class AccountInfoActivity : AppCompatActivity() {
 
     private val logoutButton: Button by lazy{
         findViewById(R.id.logoutButton)
+    }
+
+    private val changeStartDateButton: Button by lazy{
+        findViewById(R.id.changeStartDateButton)
+    }
+
+    private val editPhotoButton: Button by lazy{
+        findViewById(R.id.editPhotoButton)
+    }
+
+    private val editPartnerPhotoButton: Button by lazy{
+        findViewById(R.id.editPartnerPhotoButton)
     }
 
     private val auth: FirebaseAuth by lazy {
@@ -37,7 +56,12 @@ class AccountInfoActivity : AppCompatActivity() {
         Firebase.database.reference.child(DB_USERS)
     }
 
+    private val groupDB: DatabaseReference by lazy{
+        Firebase.database.reference.child(DB_GROUPS)
+    }
+
     var userModel: UserModel? = null
+    var groupModel: GroupModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,9 +78,8 @@ class AccountInfoActivity : AppCompatActivity() {
             finish()
         }
 
-        val nameTextView = findViewById<TextView>(R.id.nameTextView)
-        val emailTextView = findViewById<TextView>(R.id.emailTextView)
-        val phoneNumberTextView = findViewById<TextView>(R.id.phoneNumberTextView)
+
+
 /*
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
@@ -72,6 +95,21 @@ class AccountInfoActivity : AppCompatActivity() {
         }
         userDB.child(user).addValueEventListener(postListener)*/
 
+        initPhoto()
+        initGroup()
+        initDate()
+    }
+
+    private fun initPhoto(){
+
+    }
+
+    private fun initGroup(){
+        val nameTextView = findViewById<TextView>(R.id.nameTextView)
+        val emailTextView = findViewById<TextView>(R.id.emailTextView)
+        val phoneNumberTextView = findViewById<TextView>(R.id.phoneNumberTextView)
+        val nameTextViewPartner = findViewById<TextView>(R.id.nameTextViewPartner)
+
         val curruser = userDB.child(user)
         curruser.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -80,9 +118,60 @@ class AccountInfoActivity : AppCompatActivity() {
                 nameTextView.text = userModel?.name
                 emailTextView.text = userModel?.email
                 phoneNumberTextView.text = userModel?.phoneNumber
-            }
 
+                val currentGroup = groupDB.child(userModel?.groupID!!) // groupId에 접근
+                currentGroup.addValueEventListener(object: ValueEventListener{
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        groupModel = snapshot.getValue<GroupModel>()
+                        var partnerId: String?=null
+
+                        partnerId = if(user.compareTo(groupModel?.firstUserID!!) == 0){     // 내 id와 대조하여 상대의 id 찾아내기
+                            groupModel?.secondUserID.toString()
+                        }else{
+                            groupModel?.firstUserID.toString()
+                        }
+
+                        val partnerUser = userDB.child(partnerId)                       // 파트너의 id에 접근하여 이름 알아내기
+                        partnerUser.addValueEventListener(object : ValueEventListener{
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                userModel = snapshot.getValue<UserModel>()
+
+                                nameTextViewPartner.text = userModel?.name
+                            }
+                            override fun onCancelled(error: DatabaseError) {}
+                        })
+                    }
+                    override fun onCancelled(error: DatabaseError) {}
+                })
+            }
             override fun onCancelled(error: DatabaseError) {}
         })
     }
+
+
+    @SuppressLint("SetTextI18n")
+    private fun initDate(){
+        val dateTextView = findViewById<TextView>(R.id.dateTextView)
+        val startDateTextView = findViewById<TextView>(R.id.startDateTextView)
+
+        val today = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY,0)
+            set(Calendar.MINUTE,0)
+            set(Calendar.SECOND,0)
+            set(Calendar.MILLISECOND,0)
+        }.time.time
+
+        changeStartDateButton.setOnClickListener {
+
+            val cal = Calendar.getInstance()
+            val dateSetListener = DatePickerDialog.OnDateSetListener{_, year, month, dayOfMonth->
+                startDateTextView.text = "${year}.${month+1}.${dayOfMonth}~"
+            }
+            DatePickerDialog(this, dateSetListener, cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH)).show()
+
+        }
+
+    }
+
+
 }
