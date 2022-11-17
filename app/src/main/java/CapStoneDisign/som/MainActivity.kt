560360f66a09b9f9a78ba2b89c270980da05da45
mh991221 <mh991221@naver.com>
2022-11-17 20:18:22 +0900

    QR code edited

--- app/src/main/java/CapStoneDisign/som/MainActivity.kt
+++ app/src/main/java/CapStoneDisign/som/MainActivity.kt
@@ -19,7 +19,6 @@ import android.widget.CalendarView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.gms.location.*
@@ -36,10 +35,12 @@ import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.*
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.MultipartPathOverlay
 import com.naver.maps.map.overlay.MultipartPathOverlay.ColorPart
-import com.naver.maps.map.overlay.PathOverlay
 import com.naver.maps.map.util.FusedLocationSource
 import com.naver.maps.map.widget.LocationButtonView
 import java.time.LocalDate
@@ -59,6 +60,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
 
     //DB에서 받아온 경로의 좌표들 담아둘 리스트
     var memory = mutableListOf<MutableList<LatLng>>()
+
     // 경로 그릴때 필요한 색
     var memcolor = mutableListOf<ColorPart>()
 
@@ -116,7 +118,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
     private lateinit var naverMap: NaverMap
 
     private var isTracking: Int = 0
-    private var groupID: String?= null
+    private var groupID: String? = null
 
     private lateinit var locationSource: FusedLocationSource
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
@@ -138,7 +140,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
 
         calender.setOnDateChangeListener { calender, year, month, dayOfMonth ->
             // 아니 ㅅㅂ 왜 반환하는 달 값은 +1을 해줘야 하는 건대 ㅅㅂ ㅋㅋㅋㅋㅋㅋㅋㅋㅋ
-            var day:String = "${year}-${month+1}-${dayOfMonth}"
+            var day: String = "${year}-${month + 1}-${dayOfMonth}"
             Log.d("mylog", day)
             getMemory(day)
         }
@@ -154,16 +156,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        dateButton.setOnClickListener {
-            val dlg = DateQRDialog(this)
-            dlg.start()
-            dlg.setOnOKClickedListener{ content ->
-                if(content.compareTo("intent") == 0){
-                    val intent = Intent(this, QrCodeActivity::class.java)
-                    startActivityForResult(intent,486486)
-                }
-            }
-        }
 
         val currUser = userDB.child(auth.currentUser!!.uid)
         currUser.addValueEventListener(object : ValueEventListener {
@@ -175,6 +167,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
             override fun onCancelled(error: DatabaseError) {}
         })
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
@@ -184,7 +177,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
 
                     val contents = data!!.getStringExtra("groupID")
                     Log.d("QRCODEREQUESTED1", "$contents")
-                    if(data!!.getStringExtra("groupID").toString().compareTo(groupID!!) == 0){
+                    if (data!!.getStringExtra("groupID").toString().compareTo(groupID!!) == 0) {
                         isTracking = 1
                         Log.d("QRCODEREQUESTED1", "$isTracking")
                     }
@@ -214,10 +207,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
 
     @SuppressLint("MissingPermission")
     fun setUpdateLocationListener() {
-        val locationRequest = LocationRequest.create()
+        val locationRequest = LocationRequest.create().apply {
+            interval = 10000
+        }
         locationRequest.run {
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY //높은 정확도
-            interval = 1000 //1초에 한번씩 GPS 요청
+            interval = 10000 //gps 요청 시간 조정
         }
 
         locationCallback = object : LocationCallback() {
@@ -229,7 +224,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
 
                         routes[routes.lastIndex].add(LatLng(location.latitude, location.longitude))
 
-                        if(routes[routes.lastIndex].size>=3){
+                        if (routes[routes.lastIndex].size >= 3) {
                             path.coordParts = routes
                             path.map = naverMap
                         }
@@ -304,7 +299,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
                                 routes.clear()
                                 memBuf.clear()
                                 memcolor.clear()
-                                for (i: Int in 0 until routesd.size step(2)) {
+                                for (i: Int in 0 until routesd.size step (2)) {
                                     // 일단 더블형이니까, != 0.0 쓰면 또 부동 소수점 특유의 빡치는 크기비교 나올 거 같아서
                                     // 적당히 우리 나라에 있을 수 없는 좌표인 0.3 정도로 끊어둠. 이정도면 문제 생길 일 없지 않을까?
                                     // 요는, 0.0을 토큰으로 쓰기 위해 이렇게 했다는 것이다.
@@ -314,14 +309,20 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
                                         // memory에 아무것도 없으면 리스트 하나 만들어서 넣고,
                                         // 뭐라도 하나 있으면 제일 뒤의 리스트에 값 넣는다.
                                         memBuf.add(LatLng(routesd[i], routesd[i + 1]))
-                                    }
-                                    else {
+                                    } else {
                                         // 0,0이 나왔을 경우, 즉, 리스트를 끊어줘야 할 경우
                                         if (memBuf.size >= 3) {
                                             routes.add(mutableListOf<LatLng>())
                                             routes[routes.lastIndex].addAll(memBuf)
                                             memBuf.clear()
-                                            memcolor.add(MultipartPathOverlay.ColorPart(Color.RED, Color.WHITE, Color.GRAY, Color.LTGRAY))
+                                            memcolor.add(
+                                                MultipartPathOverlay.ColorPart(
+                                                    Color.RED,
+                                                    Color.WHITE,
+                                                    Color.GRAY,
+                                                    Color.LTGRAY
+                                                )
+                                            )
                                         }
                                     }
                                 }
@@ -343,8 +344,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
                                     "찾아보자 오류!!"
                                 )
                             }
-                        }
-                        else {
+                        } else {
                             Log.d("MyTAG", "No such document")
                         }
                     }
@@ -353,10 +353,29 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
                     }
                 // 새로이 적어나갈 경로를 위해 미리 만들어두는 것.
                 routes.add(mutableListOf<LatLng>())
-                memcolor.add(MultipartPathOverlay.ColorPart(Color.RED, Color.WHITE, Color.GRAY, Color.LTGRAY))
+                memcolor.add(
+                    MultipartPathOverlay.ColorPart(
+                        Color.RED,
+                        Color.WHITE,
+                        Color.GRAY,
+                        Color.LTGRAY
+                    )
+                )
                 path.colorParts = memcolor
                 checkWritingOrNot = 1
                 Log.d("MyTAG", "기록 시작")
+            }
+
+            R.id.QRIcon -> {
+                val dlg = DateQRDialog(this)
+                dlg.start()
+                dlg.setOnOKClickedListener { content ->
+                    if (content.compareTo("intent") == 0) {
+                        val intent = Intent(this, QrCodeActivity::class.java)
+                        startActivityForResult(intent, 486486)
+                    }
+                }
+
             }
             R.id.endIcon -> {
                 checkWritingOrNot = 0
@@ -493,7 +512,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
                         memory.clear()
                         memBuf.clear()
                         memcolor.clear()
-                        for (i: Int in 0 until coord.size step(2)) {
+                        for (i: Int in 0 until coord.size step (2)) {
                             // 일단 더블형이니까, != 0.0 쓰면 또 부동 소수점 특유의 빡치는 크기비교 나올 거 같아서
                             // 적당히 우리 나라에 있을 수 없는 좌표인 0.3 정도로 끊어둠. 이정도면 문제 생길 일 없지 않을까?
                             // 요는, 0.0을 토큰으로 쓰기 위해 이렇게 했다는 것이다.
@@ -503,14 +522,20 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
                                 // memory에 아무것도 없으면 리스트 하나 만들어서 넣고,
                                 // 뭐라도 하나 있으면 제일 뒤의 리스트에 값 넣는다.
                                 memBuf.add(LatLng(coord[i], coord[i + 1]))
-                            }
-                            else {
+                            } else {
                                 // 0,0이 나왔을 경우, 즉, 리스트를 끊어줘야 할 경우
                                 if (memBuf.size >= 3) {
                                     memory.add(mutableListOf<LatLng>())
                                     memory[memory.lastIndex].addAll(memBuf)
                                     memBuf.clear()
-                                    memcolor.add(MultipartPathOverlay.ColorPart(Color.RED, Color.WHITE, Color.GRAY, Color.LTGRAY))
+                                    memcolor.add(
+                                        MultipartPathOverlay.ColorPart(
+                                            Color.RED,
+                                            Color.WHITE,
+                                            Color.GRAY,
+                                            Color.LTGRAY
+                                        )
+                                    )
                                 }
                             }
                         }
@@ -532,8 +557,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
                             "찾아보자 오류!!"
                         )
                     }
-                }
-                else {
+                } else {
                     Log.d("MyTAG", "No such document")
                 }
             }
