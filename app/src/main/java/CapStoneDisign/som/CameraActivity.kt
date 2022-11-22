6fe225d0851985545a5419256db0938ae77376aa
mh991221 <mh991221@naver.com>
2022-11-22 18:06:41 +0900

    Distance Calculate for PhotoZone marker

--- app/src/main/java/CapStoneDisign/som/CameraActivity.kt
+++ app/src/main/java/CapStoneDisign/som/CameraActivity.kt
@@ -3,8 +3,10 @@ package CapStoneDisign.som
 import CapStoneDisign.som.databinding.CameraLayoutBinding
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -15,10 +17,12 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import kotlin.math.*
 
 class CameraActivity : AppCompatActivity() {
 
@@ -28,6 +32,20 @@ class CameraActivity : AppCompatActivity() {
     private var imageCapture: ImageCapture? = null
     private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
+
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null // 현재 위치를 가져오기 위한 변수
+    lateinit var mLastLocation: Location // 위치 값을 가지고 있는 객체
+    internal lateinit var mLocationRequest: LocationRequest // 위치 정보 요청의 매개변수를 저장하는
+    private val REQUEST_PERMISSION_LOCATION = 10
+
+    private var standardLatitude = 0.0
+    private var standardLongitude = 0.0
+
+    private var presentLatitude = 0.0
+    private var presentLongitude = 0.0
+    private val numberToCalculateDistance = 6372.8 * 1000
+
+    var count = 0
 
     val CAMERA_PERMISSION = arrayOf(Manifest.permission.CAMERA)
     val CAMERA_PERMISSION_REQUEST = 100
@@ -50,12 +68,76 @@ class CameraActivity : AppCompatActivity() {
 
         binding.cameraCaptureButton.setOnClickListener {
             takePhoto()
+            startLocationUpdates()
+        }
+
+        mLocationRequest =  LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
 
         outputDirectory = getOutputDirectory()
 
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
+
+    private fun startLocationUpdates() {
+
+        //FusedLocationProviderClient의 인스턴스를 생성.
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        // 기기의 위치에 관한 정기 업데이트를 요청하는 메서드 실행
+        // 지정한 루퍼 스레드(Looper.myLooper())에서 콜백(mLocationCallback)으로 위치 업데이트를 요청
+        mFusedLocationProviderClient!!.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            // 시스템에서 받은 location 정보를 onLocationChanged()에 전달
+            locationResult.lastLocation
+            onLocationChanged(locationResult.lastLocation)
+        }
+    }
+
+    // 시스템으로 부터 받은 위치정보를 화면에 갱신해주는 메소드
+    fun onLocationChanged(location: Location) {
+        mLastLocation = location
+//        text2.text = "위도 : " + mLastLocation.latitude // 갱신 된 위도
+//        text1.text = "경도 : " + mLastLocation.longitude // 갱신 된 경도
+
+        if(count == 0){
+            standardLatitude = mLastLocation.latitude
+            standardLongitude = mLastLocation.longitude
+            count++
+        }else {
+            presentLatitude = mLastLocation.latitude
+            presentLongitude = mLastLocation.longitude
+
+            val dLat = Math.toRadians(presentLatitude - standardLatitude)
+            val dLon = Math.toRadians(presentLongitude - standardLongitude)
+
+            val a = sin(dLat / 2).pow(2.0) + sin(dLon / 2).pow(2.0) * cos(Math.toRadians(presentLatitude)) * cos(Math.toRadians(standardLatitude))
+            val c = 2 * asin(sqrt(a))
+            val distance =  (numberToCalculateDistance * c).toInt()
+
+            if(distance <= 100){
+                count++
+            }else{
+                count = 1
+                standardLatitude = mLastLocation.latitude
+                standardLongitude = mLastLocation.longitude
+            }
+        }
+
+        if(count > 5){
+            /*todo (standardLatitude, standardLongitude)에 photoZone tag 를 달은 마커를 생성
+             현시점에서 바로 보여줄 필요 없음(naverMap 이랑 일일히 연결하기 귀찮을거 같음)
+             그냥 DB Marker 에 넣어만 두고 나중에 불러올때만 나타나도록 해주세요 */
+        }
+    }
+
 
     private fun checkPermission(permissions: Array<String>, permissionRequestNumber: Int) {
         val permissionResult = ContextCompat.checkSelfPermission(this, permissions[0])
